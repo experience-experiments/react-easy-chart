c293c60ce9077cab20bc6a8895e3d0e5a8cd6ec1
Nicholas Tettenborn <nicholas.tettenborn@rma-consulting.com>
2015-12-18 18:04:52 +0000

    Initial Bar chart displayed

--- modules/bar-chart/index.js
+++ modules/bar-chart/index.js
@@ -1,6 +1,52 @@
 import React from 'react';
+import scale from 'd3-scale';
+
 export default class BarChart extends React.Component {
+
     render() {
-        return <div>Bar Chart</div>;
+        const data = this.props.data;
+
+        const width = this.props.width,
+            barHeight = this.props.barHeight;
+
+        const x = scale.linear()
+            .domain([100, 300])
+            .range([0, width]);
+
+        return (
+            <svg className="chart" width={width} height={barHeight * data.length}>
+                {data.map((n, i) => {
+                        return <g key={i} transform={ `translate(0, ${barHeight * i})`}>
+                            <rect width={x(n.numbers)} height={barHeight - 1} fill="purple" />
+                        </g>;
+                    }
+                )}
+            </svg>
+        );
     }
 }
+
+BarChart.propTypes = {
+    data: React.PropTypes.array.isRequired,
+    width: React.PropTypes.number,
+    colHeight: React.PropTypes.number
+};
+
+BarChart.defaultProps = {
+    data: [{
+            "year": "1990",
+            "numbers": 200
+        }, {
+            "year": "1995",
+            "numbers": 190
+        }, {
+            "year": "2000",
+            "numbers": 130
+        },{
+        "year": "2010",
+        "numbers": 1130
+        }],
+    colHeight: 20,
+    width: 500,
+    barHeight: 20
+};
