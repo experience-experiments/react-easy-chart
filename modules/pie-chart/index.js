fcc680f223185db96b83d1dc010944fbbc6e089b
petermason <pmason@method.com>
2015-12-21 15:16:23 +0000

    minor

--- modules/pie-chart/index.js
+++ modules/pie-chart/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 export default class PieChart extends React.Component {
+
+  getData() {
+    return (
+      this.props.data.map(
+        (item, index) => {
+          return (
+            <div key={index}>
+              <h3>{item.age}</h3>
+              <p>{item.population}</p>
+            </div>
+          )
+        }
+      )
+    )
+  }
+
   render() {
-    return <div>Pie Chart</div>;
+    return (
+      <div>
+        <h1>Pie Chart</h1>
+        {this.getData()}
+        <svg />
+      </div>
+    )
   }
 }
+
+PieChart.propTypes = {
+  data: React.PropTypes.array.isRequired
+};
