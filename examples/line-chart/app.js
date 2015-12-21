2e64339d023c50bc6c4468b687863b0b0b0adec6
Harun Hasdal <harunhasdal@gmail.com>
2015-12-21 17:18:20 +0000

    Add index for modules

--- examples/line-chart/app.js
+++ examples/line-chart/app.js
@@ -1,6 +1,7 @@
-import data from './data.js';
 import React from 'react';
 import ReactDom from 'react-dom';
-import LineChart from '../../modules/line-chart';
 
-ReactDom.render(<LineChart data={data} width={600} height={400} marginLeft={10} marginTop={10}/>, document.getElementById('root'));
+import marketData from './data.js';
+import {LineChart} from 'rc-d3';
+
+ReactDom.render(<LineChart data={marketData} width={600} height={400} marginLeft={0} marginTop={0}/>, document.getElementById('root'));
