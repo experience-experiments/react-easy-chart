046e2c936c0a5885c1865841af2b14c7b950058c
petermason <pmason@method.com>
2015-12-21 14:44:14 +0000

    placeholder for piechart

--- examples/pie-chart/App.js
+++ examples/pie-chart/App.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import BarChart from 'rc-d3/pie-chart';
+import PieChart from 'rc-d3/pie-chart';
 
-ReactDOM.render(<PieChart />, document.getElementById('root'));
+const pieData = [
+  {
+    age: '<5',
+    population: 2704659
+  },
+  {
+    age: '5-13',
+    population: 4499890
+  },
+  {
+    age: '14-17',
+    population: 2159981
+  },
+  {
+    age: '18-24',
+    population: 3853788
+  },
+  {
+    age: '25-44',
+    population: 14106543
+  },
+  {
+    age: '45-64',
+    population: 8819342
+  },
+  {
+    age: '>65',
+    population: 612463
+  }
+];
+
+ReactDOM.render(<PieChart data={pieData} />, document.getElementById('root'));
