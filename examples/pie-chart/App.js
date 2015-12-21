467f21572f96a79537478f292bb5d4e4aefe645e
petermason <pmason@method.com>
2015-12-21 13:02:43 +0000

    basic structure for pie-chart component

---
+++ examples/pie-chart/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import BarChart from 'rc-d3/pie-chart';
+
+ReactDOM.render(<PieChart />, document.getElementById('root'));
---
+++ modules/pie-chart/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default class PieChart extends React.Component {
+  render() {
+    return <div>Pie Chart</div>;
+  }
+}
