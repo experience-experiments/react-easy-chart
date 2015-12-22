f4a7aacfeaa24f52295703b761432997243260cb
harunhasdal <harun.hasdal@rma-consulting.com>
2015-12-22 16:42:03 +0000

    Fix eslint errors

--- examples/bar-chart/App.js
+++ examples/bar-chart/App.js
@@ -3,65 +3,54 @@ import ReactDOM from 'react-dom';
 
 import BarChart from 'rc-d3/bar-chart';
 
-
-let mouseOverHandler = function (e) {
-    console.log(e);
-}
-
-
+const mouseOverHandler = () => {};
 
 class BarChartContainer extends React.Component {
     constructor(props) {
-        super(props);
-        this.data = this.generateData();
+      super(props);
+      this.data = this.generateData();
+    }
+
+    getRandomArbitrary(min, max) {
+      return Math.random() * (max - min) + min;
     }
 
     generateData() {
-        let data = [],
-            keys = ['A','B','C','D','E','F','G','H','I','J','K','L','M'];
+      const data = [];
+      const keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
 
-        keys.map((key, i) => {
-            data.push({key: key, value: this.getRandomArbitrary(0.1, 0)});
-        })
-        return data;
+      keys.map((key) => {
+        data.push({key: key, value: this.getRandomArbitrary(0.1, 0)});
+      });
+
+      return data;
     }
 
-
-
     turnOnSetTimeout() {
-        setInterval( this.updateData.bind(this), 500);
+      setInterval(this.updateData.bind(this), 500);
     }
 
 
     generateDataSingle() {
-        this.data[0].value = this.getRandomArbitrary(0.08, 0);
-        return this.data;
+      this.data[0].value = this.getRandomArbitrary(0.08, 0);
+      return this.data;
     }
 
-    getRandomArbitrary(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-    updateData (e) {
-        this.data = this.generateData();
-        this.forceUpdate();
+    updateData() {
+      this.data = this.generateData();
+      this.forceUpdate();
     }
 
     render() {
-        return <div>
-            <input type='button' value='reset the data' onClick={this.updateData.bind(this)}></input>
-            <input type='button' value='Turn On setTimeOut' onClick={this.turnOnSetTimeout.bind(this)}></input>
-            <BarChart
-            mouseOverHandler={mouseOverHandler}
-            data={this.data}/>
+      return (
+        <div>
+            <input type="button" value="reset the data" onClick={this.updateData.bind(this)}></input>
+            <input type="button" value="Turn On setTimeOut" onClick={this.turnOnSetTimeout.bind(this)}></input>
+            <BarChart mouseOverHandler={mouseOverHandler} data={this.data}/>
         </div>
+      );
     }
 }
 
 
-ReactDOM.render(
-    <BarChartContainer/>
-   , document.getElementById('root'));
-
-
-
+ReactDOM.render(<BarChartContainer/>, document.getElementById('root'));
--- modules/bar-chart/index.js
+++ modules/bar-chart/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import scale from 'd3-scale';
-import d3 from 'd3';
-import ReactFauxDOM from 'react-faux-dom';
+import { ordinal, linear } from 'd3-scale';
+import { select, svg, max} from 'd3';
+import { createElement } from 'react-faux-dom';
 import { Style } from 'radium';
 
 const styles = {
@@ -9,10 +9,10 @@ const styles = {
     border: 'solid silver 1px'
   },
   '.bar': {
-      fill: 'blue'
+    fill: 'blue'
   },
   '.bar:hover': {
-      fill: 'brown'
+    fill: 'brown'
   },
   '.axis': {
     font: '10px sans-serif'
@@ -23,168 +23,91 @@ const styles = {
     'shape-rendering': 'crispEdges'
   },
   'x.axis path': {
-      display: 'none'
+    display: 'none'
   }
 };
 
 
 export default class BarChart extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-
     render() {
-        let { data, margin, width, height, mouseOverHandler} = this.props;
-        width = width - margin.left - margin.right;
-        height = height - margin.top - margin.bottom;
+      const { data, margin, mouseOverHandler} = this.props;
+      let {width, height} = this.props;
+      width = width - margin.left - margin.right;
+      height = height - margin.top - margin.bottom;
 
-        let self = this;
-        let x = d3.scale.ordinal()
-            .rangeRoundBands([0, width], .1);
-        let y = d3.scale.linear()
-            .range([height, 0]);
+      const x = ordinal()
+          .rangeRoundBands([0, width], 0.1);
+      const y = linear()
+          .range([height, 0]);
 
-        let xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
+      const xAxis = svg.axis()
+          .scale(x)
+          .orient('bottom');
 
-        let yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left")
-            .ticks(10, "%");
+      const yAxis = svg.axis()
+          .scale(y)
+          .orient('left')
+          .ticks(10, '%');
 
-        let svg = d3.select(ReactFauxDOM.createElement('svg'));
+      const node = createElement('svg');
+      const selection = select(node);
 
-        svg.attr({
-                width: width + margin.left + margin.right,
-                height: height + margin.top + margin.bottom
-            });
+      selection.attr({
+        width: width + margin.left + margin.right,
+        height: height + margin.top + margin.bottom
+      });
 
-        let svgContainer = svg.append('g')
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      const svgContainer = selection.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
 
-        data.map((d,i) => {
-            x.domain(data.map(function(d) { return d.key; }));
-            y.domain([0, d3.max(data, function(d) { return d.value; })]);
+      x.domain(data.map((d) => d.key));
+      y.domain([0, max(data, (d) => d.value)]);
 
-            svgContainer.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
+      data.map(() => {
+        svgContainer.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis);
 
-            svgContainer.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Frequency");
+        svgContainer.append('g')
+            .attr('class', 'y axis')
+            .call(yAxis)
+            .append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('y', 6)
+            .attr('dy', '.71em')
+            .style('text-anchor', 'end')
+            .text('Frequency');
 
-            svgContainer.selectAll(".bar")
-                .data(data)
-                .enter().append("rect")
-                .attr("class", "bar")
-                .attr("x", function(d) { return x(d.key); })
-                .attr("width", x.rangeBand())
-                .attr("y", function(d) { return y(d.value); })
-                .attr("height", function(d) { return height - y(d.value); })
-                .on('mouseover', function(d){
-                    console.log('mouseover happened ' + d);
-                    mouseOverHandler(d);
-                });
-        });
+        svgContainer.selectAll('.bar')
+            .data(data)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', (d) => x(d.key))
+            .attr('width', x.rangeBand())
+            .attr('y', (d) => y(d.value))
+            .attr('height', (d) => height - y(d.value))
+            .on('mouseover', (d) => mouseOverHandler(d));
+      });
 
-
-        return <div className='bar-chart'>
-            <Style
-                scopeSelector=".bar-chart"
-                rules={styles}/>
-            {svg.node().toReact()}
-        </div>;
+      return (
+        <div className="bar-chart">
+          <Style scopeSelector=".bar-chart" rules={styles}/>
+          {node.toReact()}
+        </div>
+      );
     }
 }
 
 BarChart.propTypes = {
-    data: React.PropTypes.array.isRequired,
-    width: React.PropTypes.number,
-    height: React.PropTypes.number,
-    margin: React.PropTypes.object,
-    mouseOverHandler: React.PropTypes.func
+  data: React.PropTypes.array.isRequired,
+  width: React.PropTypes.number,
+  height: React.PropTypes.number,
+  margin: React.PropTypes.object,
+  mouseOverHandler: React.PropTypes.func
 };
 
 BarChart.defaultProps = {
-    margin: {top: 20, right: 20, bottom: 30, left: 40},
-    data: [
-        {
-            "key": "A",
-            "value": .08167
-        },
-        {
-            "key": "B",
-            "value": .09167
-        },{
-            "key": "C",
-            "value": .09167
-        },{
-            "key": "D",
-            "value": .09167
-        },{
-            "key": "E",
-            "value": .09167
-        },{
-            "key": "F",
-            "value": .09167
-        },{
-            "key": "G",
-            "value": .09167
-        },{
-            "key": "H",
-            "value": .09167
-        },{
-            "key": "I",
-            "value": .09167
-        },{
-            "key": "K",
-            "value": .09167
-        },{
-            "key": "L",
-            "value": .09167
-        },{
-            "key": "M",
-            "value": .09167
-        },{
-            "key": "N",
-            "value": .09167
-        },{
-            "key": "O",
-            "value": .09167
-        },{
-            "key": "P",
-            "value": .09167
-        },{
-            "key": "Q",
-            "value": .09167
-        },{
-            "key": "R",
-            "value": .09167
-        },{
-            "key": "S",
-            "value": .09167
-        },{
-            "key": "T",
-            "value": .09167
-        },{
-            "key": "U",
-            "value": .09167
-        },{
-            "key": "V",
-            "value": .09167
-        }
-    ],
-    width: 960,
-    height: 500,
-    barHeight: 20
+  margin: {top: 20, right: 20, bottom: 30, left: 40},
+  width: 960,
+  height: 500
 };
