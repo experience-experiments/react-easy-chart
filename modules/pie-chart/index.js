5c3d2cef90e5b00a89cabee8a3ee0a6bf4f6d66b
petermason <pmason@method.com>
2015-12-22 14:51:59 +0000

    basic implimentation using faux-dom

--- modules/pie-chart/index.js
+++ modules/pie-chart/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import d3 from 'd3';
+import ReactFauxDOM from 'react-faux-dom';
 
 export default class PieChart extends React.Component {
-
-  componentDidMount() {
-    this.getPie();
-  }
-
-  getPie() {
-    let {outerRadius, innerRadius } = this.props.settings;
-    let svg = d3.select('g');
-    let arc = d3.svg.arc()
-      .outerRadius(outerRadius)
-      .innerRadius(innerRadius);
-    let g = svg.selectAll(".arc")
-      .data(d3.layout.pie().value(this.getValue.bind(this))(this.props.data))
-      .enter().append("g")
-      .attr("class", "arc");
-    g.append("path")
-      .attr("d", arc)
-      .style("fill", this.getColor.bind(this));
-  }
 
   getValue(d) {
     return d.value;
@@ -30,20 +12,37 @@ export default class PieChart extends React.Component {
     return d.data.color;
   }
 
-
+  onMouseOver(e) {
+    console.log('mouseover', e);
+  }
 
   render() {
-    let { outerRadius } = this.props.settings;
+    let { outerRadius, innerRadius } = this.props.settings;
     let w = outerRadius * 2;
     let h = outerRadius * 2;
-    return (
-      <div>
-        <h1>Pie Chart</h1>
-        <svg className="chart" width={w} height={h}>
-          <g transform={ `translate(${ outerRadius }, ${ outerRadius })` }></g>
-        </svg>
-      </div>
-    )
+
+    let arc = d3.svg.arc()
+      .outerRadius(outerRadius)
+      .innerRadius(innerRadius);
+
+    let node = ReactFauxDOM.createElement('svg');
+    let svg = d3.select(node)
+      .attr('width', w)
+      .attr('height', h)
+      .append('g')
+      .attr('transform', `translate(${ outerRadius }, ${ outerRadius })`);
+
+    let g = svg.selectAll(".arc")
+      .data(d3.layout.pie().value(this.getValue.bind(this))(this.props.data))
+      .enter().append("g")
+      .attr("class", "arc")
+      .append("path")
+      .attr("d", arc)
+      .on('mouseover', this.onMouseOver.bind(this))
+      .style("fill", this.getColor.bind(this));
+
+
+    return node.toReact();
   }
 }
 
