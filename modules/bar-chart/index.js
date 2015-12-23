557b89b53c8524d3d6501a3de6cb27d742038e3a
petermason <pmason@method.com>
2015-12-23 14:43:23 +0000

    linting

--- modules/bar-chart/index.js
+++ modules/bar-chart/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 export default class BarChart extends React.Component {
-    render() {
-        return <div>Bar Chart</div>;
-    }
+  render() {
+    return <div>Bar Chart</div>;
+  }
 }
