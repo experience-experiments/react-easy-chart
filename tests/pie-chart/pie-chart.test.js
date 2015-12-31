1820babf98f5355f475af1c66f1e5da9e560bfb1
Nicholas Tettenborn <nicholas.tettenborn@rma-consulting.com>
2015-12-31 09:47:21 +0000

    Tests implemented for pie chart

--- tests/pie-chart/pie-chart.test.js
+++ tests/pie-chart/pie-chart.test.js
@@ -1,5 +1,5 @@
 /* eslint-env node, mocha */
-import {should as chaiShould} from 'chai';
+import {should as chaiShould, expect} from 'chai';
 import React from 'react';
 import TestUtils from 'react/lib/ReactTestUtils';
 import {PieChart} from 'rc-d3';
@@ -33,6 +33,32 @@ describe('PieChart component', () => {
   it('should have default values for optional properties', () => {
     const chart = TestUtils.renderIntoDocument(<PieChart data={testData}/>);
     chart.should.have.property('props');
+
+    // Data
     chart.props.data.should.have.length(2);
+
+    // Height
+    expect(chart.props).to.have.property('height', 200);
+
+    // innerHoleHeight
+    expect(chart.props).to.have.property('innerHoleHeight', 0);
+
+    // padding
+    expect(chart.props).to.have.property('padding', 2);
+
+    // hasLabels: false,
+    expect(chart.props).to.have.property('hasLabels', false);
+  });
+
+  it('should render an svg and 2 arcs', () => {
+    const shallowRenderer = TestUtils.createRenderer();
+    shallowRenderer.render(<PieChart data={testData}/>);
+    const vdom = shallowRenderer.getRenderOutput();
+    expect(vdom.type).to.equal('div');
+    const svg = vdom.props.children[1];
+    expect(svg.type).to.equal('svg');
+    const g = svg.props.children[0];
+    expect(g.props.transform).to.equal('translate(100, 100)');
+    expect(g.props.children.length).to.equal(2);
   });
 });
