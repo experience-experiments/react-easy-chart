d2d66e26f8f0f40897de40725de8af1f7541c299
Nicholas Tettenborn <nicholas.tettenborn@rma-consulting.com>
2016-01-26 17:34:47 +0000

    Chart animating but without a tween

--- modules/pie-chart/index.js
+++ modules/pie-chart/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { event as d3LastEvent, select, svg, layout, scale} from 'd3';
+import { select, selectAll, svg, layout, scale, interpolate} from 'd3';
+// import { select, layout } from 'd3';
 import { createElement } from 'react-faux-dom';
 import { Style } from 'radium';
 import merge from 'lodash.merge';
@@ -18,7 +19,6 @@ const defaultStyles = {
 };
 
 export default class PieChart extends React.Component {
-
   static get propTypes() {
     return {
       data: React.PropTypes.array.isRequired,
@@ -48,55 +48,109 @@ export default class PieChart extends React.Component {
     };
   }
 
+  constructor(props) {
+    super(props);
+    this.color = scale.category20();
+  }
+
+  componentDidMount() {
+    this.drawArc(this.props.data);
+  }
+
+  componentDidUpdate() {
+    // this.drawArc(this.props.data);
+    selectAll('.chart_lines')
+    .data(layout.pie().value((d) => d.value)(this.props.data))
+    .transition()
+    .duration(500)
+    .attr('d', this.arc);
+  }
+
+  arcTween(a) {
+    var i = interpolate(this._current, a);
+    this._current = i(0);
+    return (t) => {
+      return this.arc(i(t));
+    };
+  }
+
+  drawArc(data) {
+    // console.log(select('g'));
+    select('.svg-g-container')
+      .selectAll('.arcs')
+      .data(layout.pie().value((d) => d.value)(data))
+      .enter()
+      // .append('g')
+      .append('path')
+      .attr('d', this.arc)
+      .attr('class', 'chart_lines')
+      .style('fill', (d, i) => d.data.color ? d.data.color : this.color(i));
+
+    // .attr('d', this.arc);
+    // const g = svgNode.selectAll('.arc')
+    //   .data(layout.pie().value((d) => d.value)(this.props.data))
+    //   .enter().append('g');
+    // .datum(data)
+    // .transition()
+    // .duration(500)
+    // .attr('d', this.arc);
+  }
+
   render() {
     const {
-      mouseOverHandler,
-      mouseOutHandler,
-      mouseMoveHandler,
-      clickHandler,
+      // mouseOverHandler,
+      // mouseOutHandler,
+      // mouseMoveHandler,
+      // clickHandler,
       styles,
       innerHoleSize,
       size,
-      padding,
-      labels } = this.props;
+      padding
+      // , labels
+    } = this.props;
     const outerRadius = size / 2;
-    const arc = svg.arc()
+    this.arc = svg.arc()
       .outerRadius(outerRadius - padding)
       .innerRadius((innerHoleSize / 2) - padding);
 
-    const labelArc = svg.arc()
-      .outerRadius(outerRadius - padding - ((20 * outerRadius) / 100))
-      .innerRadius(outerRadius - padding - ((20 * outerRadius) / 100));
+    // const labelArc = svg.arc()
+    //   .outerRadius(outerRadius - padding - ((20 * outerRadius) / 100))
+    //   .innerRadius(outerRadius - padding - ((20 * outerRadius) / 100));
 
-    const color = scale.category20();
+    // const color = scale.category20();
     const node = createElement('svg');
 
-    const svgNode = select(node)
+    // const svgNode =
+    select(node)
       .attr('width', size)
       .attr('height', size)
       .append('g')
+      .attr('class', 'svg-g-container')
       .attr('transform', `translate(${ outerRadius }, ${ outerRadius })`);
 
-    const g = svgNode.selectAll('.arc')
-      .data(layout.pie().value((d) => d.value)(this.props.data))
-      .enter().append('g');
+    // svgNode
+    //   .selectAll('.arcs')
+    //   .data(layout.pie().value((d) => d.value)(this.props.data))
+    //   .enter().append('g')
+    //   .append('path')
+    //   .attr('d', this.arc)
+    //   .attr('class', 'chart_lines')
+    //   .style('fill', (d, i) => d.data.color ? d.data.color : color(i));
 
-    g.append('path')
-      .attr('d', arc)
-      .attr('class', 'chart_lines')
-      .style('fill', (d, i) => d.data.color ? d.data.color : color(i))
-      .on('mouseover', (d) => mouseOverHandler(d, d3LastEvent))
-      .on('mouseout', (d) => mouseOutHandler(d, d3LastEvent))
-      .on('mousemove', () => mouseMoveHandler(d3LastEvent))
-      .on('click', (d) => clickHandler(d, d3LastEvent));
 
-    if (labels) {
-      g.append('text')
-        .attr('transform', (d) => `translate(${labelArc.centroid(d)})`)
-        .text((d) => d.data.key)
-        .attr('class', 'chart_text')
-        .on('click', (d) => clickHandler(d, d3LastEvent));
-    }
+    // console.log(g);
+      // .on('mouseover', (d) => mouseOverHandler(d, d3LastEvent))
+      // .on('mouseout', (d) => mouseOutHandler(d, d3LastEvent))
+      // .on('mousemove', () => mouseMoveHandler(d3LastEvent))
+      // .on('click', (d) => clickHandler(d, d3LastEvent));
+
+    // if (labels) {
+    //   g.append('text')
+    //     .attr('transform', (d) => `translate(${labelArc.centroid(d)})`)
+    //     .text((d) => d.data.key)
+    //     .attr('class', 'chart_text')
+    //     .on('click', (d) => clickHandler(d, d3LastEvent));
+    // }
     const uid = Math.floor(Math.random() * new Date().getTime());
 
     return (
